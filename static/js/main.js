d286ee9c913dcda7fa38dffd986f5dc025980502
Salvatore Iovene <salvatore@iovene.com>
2013-03-28 11:46:54 +0200

    Add colorbox.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,8 @@
-
+jQuery(document).ready(function($) {
+	// Auto-handle direct jpeg links
+	$('a[href$="jpg"]').each(function() {
+        $(this).colorbox({
+            width: '100%'
+        });
+	});
+});
