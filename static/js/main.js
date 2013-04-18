51898051410777c57ce7d2f3c2596a61253b7a6d
Salvatore Iovene <salvatore@iovene.com>
2013-04-18 13:20:58 +0300

    Remove maxHeight constraint from lightbox.

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,7 @@ jQuery(document).ready(function($) {
 	// Auto-handle direct jpeg links
 	$('a[href$="jpg"]').each(function() {
         $(this).colorbox({
-            maxWidth: '100%',
-            maxHeight: '100%'
+            maxWidth: '100%'
         });
 	});
 });
