58bcb850992cef19d7c6c9c3c33b9fe8c7791d0c
Salvatore Iovene <salvatore@iovene.com>
2013-04-23 15:30:30 +0300

    Only captionize visible images.

--- static/js/jquery.captions.js
+++ static/js/jquery.captions.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// Every image referenced from a Markdown document
-	$("article img").each(function() {
+	$("article img:visible").each(function() {
 		// Let's put a caption if there is one
 		if($(this).attr("title"))
 			$(this).wrap('<figure class="image"></figure>')
