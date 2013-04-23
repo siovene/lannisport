1083a2b3d4480b94de421c1f9c1b01c2711b4951
Salvatore Iovene <salvatore@iovene.com>
2013-04-23 15:22:21 +0300

    Add Back to top link.

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,13 @@
+jQuery.fn.autohidebacktotop = function() {
+	if ($("html, body").height() <= $(window).height()) {
+		$("#autohidebacktotop").remove();
+	}
+	this.bind("click", function() {
+		$("html, body").animate({scrollTop:0});
+		return false;
+	});
+};
+
 jQuery(document).ready(function($) {
 	// Auto-handle direct jpeg links
 	$('a[href$="jpg"]').each(function() {
@@ -5,4 +15,16 @@ jQuery(document).ready(function($) {
             maxWidth: '100%'
         });
 	});
+
+    $("#back-to-top").autohidebacktotop();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $('#back-to-top:hidden').stop(true, true).fadeIn();
+        } else {
+            $('#back-to-top').stop(true, true).fadeOut();
+        }
+    });
 });
+
+
